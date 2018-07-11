308ad1d09285c2b5568821bb23a63c38b6dcc602
SuperSamson <fongpuilam@gmail.com>
2018-07-11 22:09:22 +0800

    update config file

--- content/meta/config.js
+++ content/meta/config.js
@@ -2,21 +2,21 @@ const colors = require("../../src/styles/colors");
 
 module.exports = {
   siteTitle: "Samson Lam - PersonalBlog", // <title>
-  shortSiteTitle: "PersonalBlog GatsbyJS Starter", // <title> ending for posts and pages
-  siteDescription: "PersonalBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
+  shortSiteTitle: "Samson Lam - PersonalBlog", // <title> ending for posts and pages
+  siteDescription: "Samson Lam - PersonalBlog",
+  siteUrl: "https://samsonlam.nets.hk",
   pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
   // author
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Samson Lam",
+  authorTwitterAccount: "samsonllam",
   // info
   infoTitle: "Samson Lam",
   infoTitleNote: "personal blog",
   // manifest.json
   manifestName: "Samson Lam - PersonalBlog",
-  manifestShortName: "PersonalBlog", // max 12 characters
+  manifestShortName: "SamsonBlog", // max 12 characters
   manifestStartUrl: "/",
   manifestBackgroundColor: colors.background,
   manifestThemeColor: colors.background,
